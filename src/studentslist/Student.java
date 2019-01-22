390e63190b4c4a436b790cbd37a906c43ae03eaa
Dien-Nguyen-Sheridan <nguydien@sheridancollege.ca>
2019-01-22 17:13:20 -0500

    come back to the originall

--- src/studentslist/Student.java
+++ src/studentslist/Student.java
@@ -14,7 +14,6 @@ public class Student {
    
     private String name;
     private int id;
-    private String Dob;
  
     public Student()
     {
@@ -39,27 +38,11 @@ public class Student {
     
     public void setId(int givenId)
     {
-            id = givenId;
+            this.id = givenId;
     }
     
     public int getId()
     {
         return id;
-    }
-
-    /**blue :means changed it
-     * green: new
-     * red : delete
-     * @return the Dob
-     */
-    public String getDob() {
-        return Dob;
-    }
-
-    /**
-     * @param Dob the Dob to set
-     */
-    public void setDob(String Dob) {
-        this.Dob = Dob;
     }
 }
